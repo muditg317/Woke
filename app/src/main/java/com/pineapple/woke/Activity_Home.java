727b287e25af538590e9f5d803d9a8f6ecace4f2
chough <joshua.chough@gsmst.org>
2018-12-15 10:52:32 -0500

    Start screen: - changed background animation to alpha Home screen: - added app title - added welcome header - added imageview button - added start studying intent Studying scree: - initialized Styles: - updated Colors: - updated

--- app/src/main/java/com/pineapple/woke/Activity_Home.java
+++ app/src/main/java/com/pineapple/woke/Activity_Home.java
@@ -1,5 +1,6 @@
 package com.pineapple.woke;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,12 @@ public class Activity_Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    private void startStudying() {
+        Intent intent = new Intent(this, Activity_Studying.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        startActivity(intent);
+        //finish();
     }
 }
